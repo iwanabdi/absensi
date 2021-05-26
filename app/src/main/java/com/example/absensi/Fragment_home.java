0bfcc6cf5dd19404b11a25a11bc31327617f18a2
Aldo <ronaldoyohanes4@gmail.com>
2021-05-26 21:27:13 +0700

    Aldo

--- app/src/main/java/com/example/absensi/Fragment_home.java
+++ app/src/main/java/com/example/absensi/Fragment_home.java
@@ -8,24 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-public class Homefrag extends Fragment {
-
-    public Homefrag() {
+public class Fragment_home extends Fragment {
+    public Fragment_home() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
 
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homefrag, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
