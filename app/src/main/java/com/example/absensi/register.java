8d7c9946b60459727e25578db7a2eb7a021092a1
iwanabdi <iwan.abdillah14@gmail.com>
2021-05-26 21:56:09 +0700

    regis

--- app/src/main/java/com/example/absensi/register.java
+++ app/src/main/java/com/example/absensi/register.java
@@ -2,11 +2,13 @@ package com.example.absensi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,9 +18,11 @@ public class register extends AppCompatActivity {
     EditText etnama,etnik,etpass,etconfpass;
     Spinner spjabatan;
     Button btnlgn,btnregis;
-    String[] prodi = new String[]{"Enginer", "Supervisor","Manager"};
-    ArrayList<String> listProdi;
+    String[] jab = new String[]{"Enginer", "Supervisor","Manager"};
+    ArrayList<String> listjaba;
     ArrayAdapter<String> adapterSpinner;
+    AppDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +35,35 @@ public class register extends AppCompatActivity {
         btnlgn = findViewById(R.id.btnlogin);
         btnregis = findViewById(R.id.btnregister);
         spjabatan = findViewById(R.id.spinjabatan);
-        listProdi = new ArrayList<>();
-        listProdi.addAll(Arrays.asList(prodi));
-        adapterSpinner = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, listProdi);
-        spinProdi.setAdapter(adapterSpinner);
+        listjaba = new ArrayList<>();
+        listjaba.addAll(Arrays.asList(jab));
+        adapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, listjaba);
+        spjabatan.setAdapter(adapterSpinner);
+        db = AppDatabase.getDatabase(register());
+
+    }
+
+    private class Addpegawaitask extends AsyncTask<Pegawai, Void, Void> {
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            reset();
+            Toast.makeText(this, "data berhasil ditambahkan!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Pegawai... pegawais) {
+            db.pegawaiDAO().insert(pegawais[0]);
+            return null;
+        }
+    }
+
+    public void reset() {
+        etnama.setText("");
+        etnik.setText("");
+        etpass.setText("");
+        etconfpass.setText("");
+        spjabatan.setSelected(false);
     }
 }
